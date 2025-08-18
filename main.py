5f5ef30b1dfbd372e176c688bbef4e4a3eafb349
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2025-08-19 00:11:00 +0900

    feat

--- main.py
+++ main.py
@@ -30,5 +30,9 @@ def ip_check():
     res = requests.get(url)
     return res.text
 
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
